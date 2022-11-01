4a0990f9520495cb9617b7a5f1ab133cdb63f896
Der Benji <nyctophobia@protonmail.com>
2022-11-01 10:13:37 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# yes-we-scan
-Utilize nmap with GitHub Action
+# Keep calm and yes we scan
+
+THis is a GitHub Action powered namp scanner. This scanner will apply a [vulnersCom/nmap-vulners](https://github.com/vulnersCom/nmap-vulners) scan against all hosts that are placed in `scan.txt`. 
+
+After push into the master branch and successfully scan of the targets the results will be posted as a GitHub issue.
+
+<p align="center">
+<img width="600" src="/static/yws.jpg">
+</p>
---
+++ nmap-scanner/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ISSUES_URL="https://github.com/BenjiTrapp/yes-we-scan/issues"
+
+
+function yes_we_scan() {
+    nmap  --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -sV --open -iL scan.txt --oN outputfile.txt
+}
+
+function create_gh_issue_with_scan_results() {
+    title="Yes we scanned on $(date "+%D %T")"
+    body=$(sed '1d;s/"/\\"/g;:a;N;$!ba;s/\n/\\n/g' outputfile.txt)
+    payload="{\"title\":\"$title\",\"body\":\"$body\"}"
+    curl -i -H "Authorization: token $GITHUB_TOKEN" -d "$payload" $ISSUES_URL
+}
+
+function main() {
+    yes_we_scan
+    create_gh_issue_with_scan_results
+}
+
+main
