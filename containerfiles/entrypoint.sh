2799c2352b6f967863c87c1893ed577c355c6e21
Benjamin-Yves Trapp <benji.trapp@gmail.com>
2022-11-01 23:11:13 +0100

    Update entrypoint.sh

--- containerfiles/entrypoint.sh
+++ containerfiles/entrypoint.sh
@@ -11,7 +11,7 @@ function create_gh_issue_with_scan_results() {
     title="Yes we scanned on $(date "+%D %T")"
     body=$(sed '1d;s/"/\\"/g;:a;N;$!ba;s/\n/\\n/g' /tmp/outputfile.txt)
     payload="{\"title\":\"$title\",\"body\":\"$body\"}"
-   # curl -i -H "Authorization: token $GITHUB_TOKEN" -d "$payload" $ISSUES_URL
+    curl -i -H "Authorization: token $API_TOKEN" -d "$payload" $ISSUES_URL
     env
     curl -v --request POST \
           --url https://api.github.com/repos/${REPO_URL}/issues \
