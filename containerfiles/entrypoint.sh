cd4b6525d1fbcc1362878b6341e29025c16bbfe7
Der Benji <benji.trapp@gmail.com>
2022-11-01 21:49:14 +0100

    Debug

--- containerfiles/entrypoint.sh
+++ containerfiles/entrypoint.sh
@@ -4,12 +4,16 @@ ISSUES_URL="https://github.com/BenjiTrapp/yes-we-scan/issues"
 
 
 function yes_we_scan() {
-    nmap --script=vulscan/vulscan.nse --script-args vulscandb=exploitdb.csv -sV --open -iL scan.txt --oN outputfile.txt
+    nmap --script=vulscan/vulscan.nse --script-args vulscandb=exploitdb.csv -sV --open -iL scan.txt --oN /tmp/outputfile.txt
 }
 
 function create_gh_issue_with_scan_results() {
+    echo "[INFO] Upload nmap output as issue to GitHub"
+    echo "[INFO] $ISSUES_URL"
+    cat /tmp/outputfile.txt
+
     title="Yes we scanned on $(date "+%D %T")"
-    body=$(sed '1d;s/"/\\"/g;:a;N;$!ba;s/\n/\\n/g' outputfile.txt)
+    body=$(sed '1d;s/"/\\"/g;:a;N;$!ba;s/\n/\\n/g' /tmp/outputfile.txt)
     payload="{\"title\":\"$title\",\"body\":\"$body\"}"
     curl -i -H "Authorization: token $GITHUB_TOKEN" -d "$payload" $ISSUES_URL
 }
