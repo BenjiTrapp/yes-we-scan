8bb5305f5b6b025bc8b2fdaa226e789441048442
BenjiTrapp <nyctophobia@protonmail.com>
2023-01-08 23:24:57 +0100

    cleanup

--- containerfiles/entrypoint.sh
+++ containerfiles/entrypoint.sh
@@ -4,29 +4,8 @@ function yes_we_scan() {
     nmap --script=vulscan/vulscan.nse --script-args vulscandb=exploitdb.csv -sV --open -iL scan.txt --oN /tmp/outputfile.txt
 }
 
-function create_gh_issue_with_scan_results() {
-    echo "[INFO] Upload nmap output as issue to GitHub"
-    title="Yes we scanned on $(date "+%D %T")"
-    body=$(sed '1d;s/"/\\"/g;:a;N;$!ba;s/\n/\\n/g' /tmp/outputfile.txt)
-    payload="{\"title\":\"$title\",\"body\":\"$body\"}"
-   
-    echo "GITHUB_TOKEN:  $GITHUB_TOKEN"
-    echo "REPO_URL: $REPO_URL"
-
-    curl --request POST \
-          --url https://api.github.com/repos/${REPO_URL}/issues \
-          --header 'authorization: Bearer ${GITHUB_TOKEN}' \
-          --header 'content-type: application/json' \
-          --data '{
-                "title": "Yes we scanned",
-                "body": "Test"
-            }' \
-          --fail  
-}
-
 function main() {
     yes_we_scan
-    #create_gh_issue_with_scan_results
 }
 
 main
